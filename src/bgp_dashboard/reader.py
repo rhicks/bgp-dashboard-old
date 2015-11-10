3e1f5f4ce17cae9b5d18a161e01ee7a41d807626
Richard Hicks <rhicks@nero.net>
2015-11-09 17:41:31 -0800

    initial stab at reading ipv4 data from a file

--- src/bgp_dashboard/reader.py
+++ src/bgp_dashboard/reader.py
@@ -1,19 +1,26 @@
 class FileReaderIPv4:
     """Read 'show ip bgp' data from a text file"""
-    pass
+    def get_data(filename):
+        with open(filename, 'r') as data_file:
+            for line in data_file:
+                yield tuple(line.split())
+
 
 class FileReaderIPv6:
     """Read 'show ip bgp ipv6 unicast' data from a text file"""
     pass
 
+
 class RouterReaderIPv4:
     """Read 'show ip bgp' data from a router login session"""
     pass
+
 
 class RouterReaderIPv6:
     """Read 'show ip bgp ipv6 unicast' data from a router login session"""
     pass
 
+
 class BMPReader:
     """Read BGP updates from a BGP Monitoring Protocol session"""
-    pass    
+    pass
--- src/bgp_dashboard/tests/test_reader.py
+++ src/bgp_dashboard/tests/test_reader.py
@@ -1,1 +1,15 @@
+import unittest
 from ..reader import FileReaderIPv4
+
+
+class FileReaderIPv4Test(unittest.TestCase):
+
+    # def test_file_should_exist_or_throw_error(self):
+    #     pass
+
+    def test_should_open_the_given_filename_to_get_data(self):
+        self.data = FileReaderIPv4.get_data("../bgp-data.txt")
+        for line in self.data:
+            if len(line) > 0:
+                print(line[1])
+     #self.assertIs(self.data, tuple)
