22d01e536a393ce18db56973b6bfaeddb135862b
Richard Hicks <rhicks@nero.net>
2015-11-10 13:56:02 -0800

    clean up parsing of ipv4 data from a txt file and start real testing of the data provided

--- src/bgp_dashboard/reader.py
+++ src/bgp_dashboard/reader.py
@@ -1,6 +1,32 @@
 class FileReaderIPv4:
-    """Read 'show ip bgp' data from a text file"""
-    def get_data(filename):
+
+    def __build_data(lines):
+        for route in lines:
+            if "/" in route[1]:
+                ipv4_prefix = route[1]
+            if "/" in route[2]:
+                ipv4_prefix = route[2]
+            if ">" in route[0]:
+                asn = route[-2]
+                if "/" in route[1]:
+                    next_hop_asn = route[6]
+                    weight = route[5]
+                    local_pref = route[4]
+                    metric = route[3]
+                    next_hop_ipv4 = route[2]
+                    as_path = route[6:-1]
+                else:
+                    next_hop_asn = route[5]
+                    weight = route[4]
+                    local_pref = route[3]
+                    metric = route[2]
+                    next_hop_ipv4 = route[1]
+                    as_path = route[5:-1]
+
+        return(ipv4_prefix, asn, next_hop_asn, weight, local_pref, metric, next_hop_ipv4, as_path)
+
+    def get_data(self, filename):
+        """Read 'show ip bgp' data from a text file"""
         with open(filename, 'r') as data_file:
             lines = []
             for line in data_file:
@@ -10,13 +36,11 @@ class FileReaderIPv4:
                         lines.append(tuple(line.split()))
                         continue
                     else:
-                        yield lines
+                        yield FileReaderIPv4.__build_data(lines)
                         lines = []
                         lines.append(tuple(line.split()))
                 elif line.startswith("*"):
                     lines.append(tuple(line.split()))
-                # yield tuple(line.split())
-                # yield lines
 
 
 class FileReaderIPv6:
--- src/bgp_dashboard/tests/test_reader.py
+++ src/bgp_dashboard/tests/test_reader.py
@@ -4,27 +4,12 @@ from ..reader import FileReaderIPv4
 
 class FileReaderIPv4Test(unittest.TestCase):
 
-    # def test_file_should_exist_or_throw_error(self):
-    #     pass
-
-    def test_should_open_the_given_filename_to_get_data(self):
-        self.data = FileReaderIPv4.get_data("../bgp-data.txt")
-        print()
-        for line in self.data:
-            #print(line)
-            for route in line:
-                if "/" in route[1]:
-                    print(route[1]) # network
-                if "/" in route[2]:
-                    print(route[2]) # network
-                if ">" in route[0]:
-                    print(route[-2]) # destination asn
-                    if "/" in route[1]:
-                        print(route[6]) # next_hop_asn
-                    else:
-                        print(route[5]) # next_hop_asn
-                # print(route)
-                # break
-            # if len(line) > 0:
-            #     print(line[1])
-     #self.assertIs(self.data, tuple)
+    def test_reader_should_return_a_tuple_of_data(self):
+        # format: ipv4_prefix, dest-asn, next-hop-asn,
+        #         weight, local_pref, metric, next_hop_ipv4, tuple(as_path)
+        self.data = FileReaderIPv4.get_data(self, "../bgp-data.txt")
+        test_data = next(self.data)  # get first tuple from the list to compare
+        self.assertTupleEqual(('1.0.0.0/24', '15169', '15169', '0',
+                               '1000', '0', '198.32.195.34', ('15169',)), test_data)
+        # for line in self.data:
+        #     print(line)
