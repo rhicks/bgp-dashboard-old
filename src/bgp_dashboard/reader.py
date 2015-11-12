a2a4c9746b7c8675388f1aa6cd6f4230a0197d24
Richard Hicks <rhicks@nero.net>
2015-11-11 21:05:12 -0800

    hacking on the reader object: add additional error checking to reader

--- src/bgp_dashboard/reader.py
+++ src/bgp_dashboard/reader.py
@@ -1,3 +1,6 @@
+# import re
+
+
 class FileReaderIPv4:
     """"Read 'show ip bgp' data from a text file and return a tuple to be processed"""
 
@@ -8,7 +11,7 @@ class FileReaderIPv4:
                 ipv4_prefix = route[1]
             if "/" in route[2]:
                 ipv4_prefix = route[2]
-            if ">" in route[0]:
+            if ">" in route[0] and len(route) > 2:
                 asn = route[-2]
                 if "/" in route[1]:
                     next_hop_asn = route[6]
@@ -26,22 +29,32 @@ class FileReaderIPv4:
                     as_path = route[5:-1]
 
         return(ipv4_prefix, asn, next_hop_asn, weight, local_pref, metric, next_hop_ipv4, as_path)
+        # return(ipv4_prefix, weight, local_pref, metric, next_hop_ipv4,
+        # as_path)
+
+    def __parse_input(raw_data):
+        # ipv4_pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+        lines = []
+        for line in raw_data:
+            line = line.lstrip().rstrip()
+            if "/" in line:
+                if len(lines) == 0:
+                    lines.append(tuple(line.split()))
+                    continue
+                else:
+                    return FileReaderIPv4.__build_data(lines)
+                    lines = []
+                    lines.append(tuple(line.split()))
+            elif (line.startswith("*") or line.startswith("r i") or line.startswith("r>i")):
+                lines.append(tuple(line.split()))
 
     def get_data(self, filename):
-        with open(filename, 'r') as data_file:
-            lines = []
-            for line in data_file:
-                line = line.lstrip().rstrip()
-                if "/" in line:
-                    if len(lines) == 0:
-                        lines.append(tuple(line.split()))
-                        continue
-                    else:
-                        yield FileReaderIPv4.__build_data(lines)
-                        lines = []
-                        lines.append(tuple(line.split()))
-                elif line.startswith("*"):
-                    lines.append(tuple(line.split()))
+        try:
+            with open(filename, 'r') as data_file:
+                yield FileReaderIPv4.__parse_input(data_file)
+        except:
+            raise EnvironmentError("Something wrong with input file")
+        data_file.close()
 
 
 class FileReaderIPv6:
--- src/bgp_dashboard/tests/test_reader.py
+++ src/bgp_dashboard/tests/test_reader.py
@@ -1,15 +1,42 @@
 import unittest
 from ..reader import FileReaderIPv4
+from ..autonomoussystem import AutonomousSystem
+from collections import namedtuple
 
 
 class FileReaderIPv4Test(unittest.TestCase):
+
+    def test_bad_filename_should_return_FileNotFoundError(self):
+            with self.assertRaises(EnvironmentError):
+                self.data = FileReaderIPv4.get_data(self, "../bgp-dat.txt")
 
     def test_reader_should_return_a_tuple_of_data(self):
         # format: ipv4_prefix, dest-asn, next-hop-asn,
         #         weight, local_pref, metric, next_hop_ipv4, tuple(as_path)
         self.data = FileReaderIPv4.get_data(self, "../bgp-data.txt")
         test_data = next(self.data)  # get first tuple from the list to compare
-        self.assertTupleEqual(('1.0.0.0/24', '15169', '15169', '0',
-                               '1000', '0', '198.32.195.34', ('15169',)), test_data)
+        self.assertTupleEqual(
+            ('1.0.0.0/24', '15169', '15169', '0', '1000', '0', '198.32.195.34', ('15169',)), test_data)
         # for line in self.data:
         #     print(line)
+
+    def test_should_be_able_to_create_AutonomousSystem_object_from_data(self):
+        self.data = FileReaderIPv4.get_data(self, "../bgp-data.txt")
+        test_data = next(self.data)
+        self.testasn = AutonomousSystem(test_data[1])
+        self.assertIsInstance(self.testasn, AutonomousSystem)
+
+    # def test_print_lines(self):
+    #     self.data = FileReaderIPv4.get_data(self, "../bgp-dat.txt")
+    #     Route = namedtuple('Route',
+    #                         'ipv4_prefix asn next_hop_asn weight local_pref metric next_hop_ipv4 as_path')
+    #     for line in self.data:
+    #         myRoute = Route(ipv4_prefix=line[0],
+    #                         asn=line[1],
+    #                         next_hop_asn=line[2],
+    #                         weight=line[3],
+    #                         local_pref=line[4],
+    #                         metric=line[5],
+    #                         next_hop_ipv4=line[6],
+    #                         as_path=line[7])
+    #         print(myRoute)
