0bc1229e056add14300b9610aef42b94664ba43e
Richard Hicks <rhicks@nero.net>
2015-11-12 17:51:03 -0800

    switch to slices to parse data from "show ip bgp"

--- src/bgp_dashboard/reader.py
+++ src/bgp_dashboard/reader.py
@@ -1,61 +1,77 @@
-# import re
-
+from collections import namedtuple
 
 class FileReaderIPv4:
     """"Read 'show ip bgp' data from a text file and return a tuple to be processed"""
 
-    def __build_data(lines):
-        # ugly code to be refactored... someday
-        for route in lines:
-            if "/" in route[1]:
-                ipv4_prefix = route[1]
-            if "/" in route[2]:
-                ipv4_prefix = route[2]
-            if ">" in route[0] and len(route) > 2:
-                asn = route[-2]
-                if "/" in route[1]:
-                    next_hop_asn = route[6]
-                    weight = route[5]
-                    local_pref = route[4]
-                    metric = route[3]
-                    next_hop_ipv4 = route[2]
-                    as_path = route[6:-1]
-                else:
-                    next_hop_asn = route[5]
-                    weight = route[4]
-                    local_pref = route[3]
-                    metric = route[2]
-                    next_hop_ipv4 = route[1]
-                    as_path = route[5:-1]
+    def __init__(self, filename):
+        self.filename = filename
+        self.previous_line = None
+        self.STATUS_START = 0
+        self.STATUS_END = 3
+        self.NETWORK_START = 4
+        self.NETWORK_END = 21
+        self.NEXTHOP_START = 21
+        self.NEXTHOP_END = 40
+        self.METRIC_START = 41
+        self.METRIC_END = 46
+        self.LOCALPREF_START = 47
+        self.LOCALPREF_END = 55
+        self.WEIGHT_START = 56
+        self.WEIGHT_END = 61
+        self.PATH_START = 62
+        self.PATH_END = -2
+        self.ORIGN = -1
 
-        return(ipv4_prefix, asn, next_hop_asn, weight, local_pref, metric, next_hop_ipv4, as_path)
-        # return(ipv4_prefix, weight, local_pref, metric, next_hop_ipv4,
-        # as_path)
+    def __parse_line(self, line, double_line):
+        if double_line:
+            self.NETWORK_END += 1
+            self.NEXTHOP_START += 1
+            self.NEXTHOP_END += 1
+            self.METRIC_START += 1
+            self.METRIC_END += 1
+            self.LOCALPREF_START += 1
+            self.LOCALPREF_END += 1
+            self.WEIGHT_START += 1
+            self.WEIGHT_END += 1
+            self.PATH_START += 1
 
-    def __parse_input(raw_data):
-        # ipv4_pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
-        lines = []
-        for line in raw_data:
-            line = line.lstrip().rstrip()
-            if "/" in line:
-                if len(lines) == 0:
-                    lines.append(tuple(line.split()))
-                    continue
-                else:
-                    return FileReaderIPv4.__build_data(lines)
-                    lines = []
-                    lines.append(tuple(line.split()))
-            elif (line.startswith("*") or line.startswith("r i") or line.startswith("r>i")):
-                lines.append(tuple(line.split()))
+        status = line[self.STATUS_START:self.STATUS_END].lstrip().rstrip()
+        ipv4_prefix = line[self.NETWORK_START:self.NETWORK_END].lstrip().rstrip()
+        next_hop_ipv4 = line[self.NEXTHOP_START:self.NEXTHOP_END].lstrip().rstrip()
+        metric = line[self.METRIC_START:self.METRIC_END].lstrip().rstrip()
+        local_pref = line[self.LOCALPREF_START:self.LOCALPREF_END].lstrip().rstrip()
+        weight = line[self.WEIGHT_START:self.WEIGHT_END].lstrip().rstrip()
+        as_path = line[self.PATH_START:self.PATH_END].lstrip().rstrip()
+        origin = line[self.ORIGN].lstrip().rstrip()
 
-    def get_data(self, filename):
-        with open(filename, 'r') as data_file:
+        return(status, ipv4_prefix, next_hop_ipv4, metric, local_pref, weight, as_path, origin)
+
+    def __parse_input(self, line):
+        as_path = line[self.PATH_START:self.PATH_END].lstrip().rstrip()
+        ipv4_prefix = line[self.NETWORK_START:self.NETWORK_END].lstrip().rstrip()
+
+        if as_path:
+            self.previous_line = None
+            return(self.__parse_line(line, False))
+        else:
+            if self.previous_line == None:
+                self.previous_line = line
+            else:
+                line = self.previous_line + " " + line
+                return(self.__parse_line(line, True))
+
+    def get_data(self):
+        real_data_begins = False
+        with open(self.filename, 'r') as data_file:
             lines = []
             for line in data_file:
                 line = line.lstrip().rstrip()
-                print(line[0:3])
-                print(line[4:21])
-                print(line[21:42])
+                if real_data_begins:
+                    yield self.__parse_input(line)
+                elif (line.startswith("*") or line.startswith("r i") or line.startswith("r>i")):
+                    real_data_begins = True
+                    yield self.__parse_input(line)
+
 
 class FileReaderIPv6:
     """Read 'show ip bgp ipv6 unicast' data from a text file"""
