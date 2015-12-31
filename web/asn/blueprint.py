a33fd1a3f990f43138c97ad1d279ea0fb93b4eb7
Richard Hicks <rhicks@nero.net>
2015-12-31 15:49:48 -0800

    add gentella admin theme. implement a real homepage

--- web/asn/blueprint.py
+++ web/asn/blueprint.py
@@ -7,4 +7,5 @@ asn = Blueprint('asn', __name__, template_folder='templates')
 @asn.route('/')
 def index():
     asn = AutonomousSystem.query.order_by(AutonomousSystem.asn.asc())
+    # autonomoussystems = db.session.query(AutonomousSystem)
     return object_list('asn/index.html', asn)
--- web/views.py
+++ web/views.py
@@ -3,17 +3,22 @@ from app import app
 from app import db
 from models import AutonomousSystem, Prefix
 from sqlalchemy import func, distinct
+from helpers import object_list
 
 @app.route('/')
 def homepage():
-    autonomoussystems = AutonomousSystem.query.order_by(AutonomousSystem.asn.asc())
-    prefixes = Prefix.query.order_by(Prefix.ip_version.asc())
-    ipv4_prefixes = Prefix.query.filter(Prefix.ip_version == 4)
-    ipv6_prefixes = Prefix.query.filter(Prefix.ip_version == 6)
-    peers = db.session.query(Prefix.next_hop_asn.distinct()).order_by(Prefix.next_hop_asn)
+    autonomoussystems = db.session.query(AutonomousSystem.asn, AutonomousSystem.name)
+    prefixes = db.session.query(Prefix.ip_version)
+    ipv4_prefixes = prefixes.filter(Prefix.ip_version == 4)
+    ipv6_prefixes = prefixes.filter(Prefix.ip_version == 6)
+    nexthop_asns = db.session.query(Prefix.next_hop_asn.distinct())
+    nexthop_ips  = db.session.query(Prefix.next_hop_ip.distinct())
+    peers = autonomoussystems.filter(AutonomousSystem.asn.in_(nexthop_asns))
+
     return render_template('home.html',
                             autonomoussystems=autonomoussystems,
                             prefixes=prefixes,
                             ipv4_prefixes=ipv4_prefixes,
                             ipv6_prefixes=ipv6_prefixes,
+                            nexthop_ips=nexthop_ips,
                             peers=peers)
